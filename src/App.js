c12f39021d3b55ed46d0ac760a5a9eb4a10c605f
rjmortel <rururonna@gmail.com>
2023-10-26 00:56:33 +0800

    adding font awesome

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Home from "./components/home";
